306cd2756ed5f70dc1de7236b8d4fb46c95f29e2
mwimer1 <macrae@getcorso.com>
2024-07-16 11:48:37 -0500

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -1,16 +1,24 @@
 export default {
   editor: {
     label: {
-      en: "My Element",
+      en: "My Table Component", // Label for the component in the WeWeb editor
     },
   },
   properties: {
-    textColor: {
+    data: {
       label: {
-        en: "Text color",
+        en: "Data Source", // Label for the property in the editor
       },
-      type: "Color",
-      defaultValue: "#F23636",
+      type: "Array", // Type of property (in this case, an array of data)
+      defaultValue: [], // Default value if not specified
     },
+    columns: {
+      label: {
+        en: "Columns Configuration", // Label for the property in the editor
+      },
+      type: "Array", // Type of property (array)
+      defaultValue: [], // Default value
+    },
+    // Add more properties as needed for your table component
   },
 };
